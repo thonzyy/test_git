3031a423d8394404838ff0234bf8948de6323f9e
thonzyy <tuna5127@icloud.com>
2023-08-22 12:40:52 +0900

    머지2

--- test_git/src/kr/co/sist/test/Ldw.java
+++ test_git/src/kr/co/sist/test/Ldw.java
@@ -5,6 +5,8 @@ public class Ldw {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("이것은 신기능 추가");
+		System.out.println("대포동 미사일");
+		System.out.println("임태균의 미소");
 	}
 
 }
